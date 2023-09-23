1b3c97e86fbbec0f05f2a1999511bac13237b8c3
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2023-09-23 21:22:57 +0530

    Created public and private routes

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyC6YeFIoaiZow4KNqJaRMk9uwk7xaUsJZw',
+  authDomain: 'chatroom-aeb2d.firebaseapp.com',
+  projectId: 'chatroom-aeb2d',
+  storageBucket: 'chatroom-aeb2d.appspot.com',
+  messagingSenderId: '517789897938',
+  appId: '1:517789897938:web:a5ad712fcaaacc74e0c90b',
+};
+
+const app = firebase.initializeApp(config);
