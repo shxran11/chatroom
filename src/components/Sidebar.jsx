c92bcd9ae5c8c41064b76d8d370e25007cec4894
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2023-09-26 23:13:19 +0530

    Added button to create new room

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import CreateRoomBtnModal from './dashboard/CreateRoomBtnModal';
 import DashboardToggole from './dashboard/DashboardToggle';
 
 const Sidebar = () => {
@@ -5,6 +6,7 @@ const Sidebar = () => {
     <div className="h-100 pt-2">
       <div>
         <DashboardToggole />
+        <CreateRoomBtnModal />
       </div>
       bottom
     </div>
