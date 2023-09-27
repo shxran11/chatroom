ddbf4e80e95a2174a70d652422a6ab3f69d48d7e
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2023-09-27 10:36:33 +0530

    modified the length of scroll bar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,27 @@
+import { Divider } from 'rsuite';
 import CreateRoomBtnModal from './dashboard/CreateRoomBtnModal';
 import DashboardToggole from './dashboard/DashboardToggle';
+import ChatRoomList from './rooms/ChatRoomList';
+import { useEffect, useRef, useState } from 'react';
 
 const Sidebar = () => {
+  const topSidebarRef = useRef();
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (topSidebarRef.current) {
+      setHeight(topSidebarRef.current.scrollHeight);
+    }
+  }, [topSidebarRef]);
+
   return (
     <div className="h-100 pt-2">
-      <div>
+      <div ref={topSidebarRef}>
         <DashboardToggole />
         <CreateRoomBtnModal />
+        <Divider>Join conversation</Divider>
       </div>
-      bottom
+      <ChatRoomList aboveElHeight={height} />
     </div>
   );
 };
