a89b4adc173b4326c4bb0e2cb4133a080178a841
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2023-09-28 12:31:01 +0530

    created layout for chat page

--- src/pages/home/Chat.jsx
+++ src/pages/home/Chat.jsx
@@ -1,5 +1,38 @@
+import ChatTop from '../../components/chatWindow/top';
+import Messages from '../../components/chatWindow/messages';
+import ChatBottom from '../../components/chatWindow/bottom';
+import { useParams } from 'react-router';
+import { useRooms } from '../../context/RoomsContext';
+import { Loader } from 'rsuite';
+
 const Chat = () => {
-  return <div>hehe</div>;
+  const { chatId } = useParams();
+
+  const rooms = useRooms();
+
+  if (!rooms) {
+    return <Loader center vertical size="md" content="Loading" speed="slow" />;
+  }
+
+  const currRoom = rooms.find(room => room.id === chatId);
+
+  if (!currRoom) {
+    return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
+  }
+
+  return (
+    <>
+      <div className="chat-top">
+        <ChatTop />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="chat-bottom">
+        <ChatBottom />
+      </div>
+    </>
+  );
 };
 
 export default Chat;
