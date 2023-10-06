6f274f9c4551658786f1428aef42bb5fd2462dba
shxran11 <saranya.podder.fiem.cse21.@teamfuture.in>
2023-10-06 22:18:26 +0530

    isplayed realtime presence

--- src/components/chatWindow/messages/MessageItem.jsx
+++ src/components/chatWindow/messages/MessageItem.jsx
@@ -1,6 +1,7 @@
 import TimeAgo from 'timeago-react';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
+import PresenceDot from '../../PresenceDot';
 
 const MessageItem = ({ message }) => {
   const { author, createdAt, text } = message;
@@ -8,6 +9,8 @@ const MessageItem = ({ message }) => {
   return (
     <li className="padded mb-1">
       <div className="d-flex align-items-center font-bolder mb-1">
+        <PresenceDot uid={author.uid} />
+
         <ProfileAvatar
           src={author.avatar}
           name={author.name}
